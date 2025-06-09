4a391cc7cf30f20e25a73c2671a978d3e9d057fc
joleDocusketch <josip@email.com>
2025-06-09 12:49:11 +0200

    Initial commit

---
+++ src/e2e/pages/ProjectsPage.ts
@@ -0,0 +1,19 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class ProjectsPage {
+
+    readonly page: Page;
+    readonly projectsLabel: Locator;
+    readonly createProjectsButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.projectsLabel = page.getByText('Active projects');
+        this.createProjectsButton = page.getByText('Create project');
+    };
+
+    async loadPage() {
+        await expect(this.projectsLabel).toBeVisible();
+        await expect(this.createProjectsButton).toBeVisible();
+    }
+};
