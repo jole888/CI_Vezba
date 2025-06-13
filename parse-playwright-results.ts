f2ed9ec43c574c05e72713fc0b74cdf02ebd869e
jole888 <josip.herceg@yahoo.com>
2025-06-13 14:36:55 +0200

    x

--- parse-playwright-results.ts
+++ parse-playwright-results.ts
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
 interface Test {
   outcome: "expected" | "unexpected" | "skipped" | "flaky";
